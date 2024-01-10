f1a5e7551b3d449a6805d5ff9c67ceee4fababdb
Imaad08 <imaadmuzaffer@gmail.com>
2024-01-10 12:38:19 -0800

    finished

--- components/recipe/RecipeSteps.js
+++ components/recipe/RecipeSteps.js
@@ -4,9 +4,7 @@ import Image from 'next/image';
 import FlotiqImage from '../../lib/FlotiqImage';
 
 const RecipeSteps = ({ additionalClass, steps, headerText }) => (
-    <div className={['flex flex-wrap max-w-7xl mx-auto '
-    + 'bg-white py-5', ...additionalClass].join(' ')}
-    >
+    <div className={['flex flex-wrap max-w-7xl mx-auto bg-white py-5', ...additionalClass].join(' ')}>
         <Header level={4} additionalClasses={['uppercase font-normal px-5 pb-5 md:px-10']}>
             {headerText}
         </Header>
@@ -15,21 +13,22 @@ const RecipeSteps = ({ additionalClass, steps, headerText }) => (
                 <Paragraph additionalClasses={['px-5 md:px-10 pb-5 md:px-14']}>
                     {`${index + 1}. ${step.step}`}
                 </Paragraph>
-                {step.image && step.image[0]
-                  && (
-                      <Image
-                          src={FlotiqImage.getSrc(step.image?.[0], 0, 0)}
-                          width={step.image[0].width}
-                          height={step.image[0].height}
-                          objectFit="cover"
-                          layout="responsive"
-                          alt={step.step}
-                          className="w-full pb-5 md:pb-10"
-                      />
-                  )}
+                {step.image && step.image[0] && (
+                    <div className="image-container w-full max-w-[400px] mx-auto pb-5 md:pb-10">
+                        <Image
+                            src={FlotiqImage.getSrc(step.image?.[0], 0, 0)}
+                            height={200}
+                            width={200}
+                            objectFit="cover"
+                            layout="responsive"
+                            alt={step.step}
+                            className="w-full"
+                        />
+                    </div>
+                )}
             </div>
         ))}
     </div>
-)
+);
 
 export default RecipeSteps;
